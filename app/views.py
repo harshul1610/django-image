0da380af6510b0f59c866d4ecd08adeff2db3844
harshul1610 <harshulrobo@gmail.com>
2016-02-20 18:08:49 +0530

    azure datablob storage working

--- app/views.py
+++ app/views.py
@@ -1,11 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 from .models import filemodel
 from .forms import fileform
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
-from azure.storage.blob import BlobService
+from azure.storage.blob import BlockBlobService
+from azure.storage.blob import PublicAccess
+from azure.storage.blob import ContentSettings
+
+myaccount='tofim'
+mykey='5jb1QMcmwb0BYFPeJwfXpw6afn27MXw6MH37BWHSxRYUfWwwaVQK7DzGEN1ygCkxFti20sDGjWUg2pFH3HVHCQ=='
 
 def index(request):
     context = RequestContext(request)
@@ -15,12 +20,25 @@ def index(request):
             modelobj = formobj.save(commit=False)
             modelobj.image=request.FILES['image']
             modelobj.save()
-            
-            blob_service = BlobService(account_name='myaccount', account_key='mykey')
-            try:
-                blob_service.create_container('mycontainer', x_ms_blob_public_access='container')
-            except:
-                blob_service.set_container_acl('mycontainer', x_ms_blob_public_access='container')
+
+            mp3= request.FILES['image']
+
+            block_blob_service = BlockBlobService(account_name=myaccount, account_key=mykey)
+            block_blob_service.create_container('harshultest2', public_access=PublicAccess.Container)
+            block_blob_service.set_container_acl('harshultest2', public_access=PublicAccess.Container)
+
+            with open('t.png','wb+') as formfile:
+                for chunk in mp3.chunks():
+                    formfile.write(chunk)
+
+            block_blob_service.create_blob_from_path(
+                'harshultest2',
+                str(request.FILES['image']),
+                't.png',
+
+            content_settings=ContentSettings(content_type='image')
+                )
+
             return HttpResponseRedirect(reverse('list'))
     else:
         formobj = fileform()
